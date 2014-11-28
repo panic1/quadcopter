a5a1ce50696b924d7f965475b4bd9cdb499388b5
Nicky Geerts <nicky.geerts@gmail.com>
2014-11-29 00:05:44 +0100

    Attempting to write a hello world application for both the remote controller and the quadcopter control application

---
+++ src/remote/pc_app/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 28, 2014
+ *      Author: Nicky Geerts
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cout << "hello world" << endl;
+}
+
