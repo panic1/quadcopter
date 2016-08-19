ec63f40a5e1def3c0d8c0f02b5f1299541c41691
Nicky Geerts <nicky.geerts@gmail.com>
2016-08-19 13:35:28 +0200

    Remove BlackLib dependency in favour of own drivers.

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -10,12 +10,11 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include "BlackPWM/BlackPWM.h"
-#include "BlackI2C/BlackI2C.h"
+
+#include "hal/pwm/pwm_beagleboneblack.hpp"
+#include "hal/i2c/i2c.hpp"
 
 using namespace std;
-
-const static uint64_t period_time = 20000000; // 20ms in ns
 
 void test_pwm();
 void test_i2c();
@@ -29,46 +28,48 @@ int main(void)
 
 void test_pwm()
 {
-	float duty_cycle = 5.0;
+	PWM* myPwm1 = new PWM_beagleboneblack(0);
+	PWM* myPwm2 = new PWM_beagleboneblack(1);
+	PWM* myPwm3 = new PWM_beagleboneblack(2);
+	PWM* myPwm4 = new PWM_beagleboneblack(3);
 
-	BlackLib::BlackPWM pwm_motor1(BlackLib::EHRPWM1A);
-	pwm_motor1.setPeriodTime(period_time);
-	pwm_motor1.setDutyPercent(duty_cycle);
+	myPwm1->enable();
+	myPwm2->enable();
+	myPwm3->enable();
+	myPwm4->enable();
 
-	BlackLib::BlackPWM pwm_motor2(BlackLib::EHRPWM1B);
-	pwm_motor2.setPeriodTime(period_time);
-	pwm_motor2.setDutyPercent(duty_cycle);
+	myPwm1->set_period_ns(20000000);
+	myPwm2->set_period_ns(20000000);
+	myPwm3->set_period_ns(20000000);
+	myPwm4->set_period_ns(20000000);
 
-	BlackLib::BlackPWM pwm_motor3(BlackLib::EHRPWM2B);
-	pwm_motor3.setPeriodTime(period_time);
-	pwm_motor3.setDutyPercent(duty_cycle);
+	int duty_cycle = 1150000;
+	myPwm1->set_duty_cycle_ns(duty_cycle);
+	myPwm2->set_duty_cycle_ns(duty_cycle);
+	myPwm3->set_duty_cycle_ns(duty_cycle);
+	myPwm4->set_duty_cycle_ns(duty_cycle);
 
-	BlackLib::BlackPWM pwm_motor4(BlackLib::EHRPWM2A);
-	pwm_motor4.setPeriodTime(period_time);
-	pwm_motor4.setDutyPercent(duty_cycle);
-
+	sleep(5);
 	while (true)
 	{
 		usleep(100000);
 
-		duty_cycle += 0.01;
-		if (duty_cycle >= 6.3) duty_cycle = 5.0;
+		duty_cycle += 1000;
+		if (duty_cycle >= 1300000) duty_cycle = 1150000;
 
-	    std::cout << "Duty Cycle: " << duty_cycle << std::endl;
+		std::cout << "Duty Cycle: " << duty_cycle << std::endl;
 
-		pwm_motor1.setDutyPercent(duty_cycle);
-		pwm_motor2.setDutyPercent(duty_cycle);
-		pwm_motor3.setDutyPercent(duty_cycle);
-		pwm_motor4.setDutyPercent(duty_cycle);
+		myPwm1->set_duty_cycle_ns(duty_cycle);
+		myPwm2->set_duty_cycle_ns(duty_cycle);
+		myPwm3->set_duty_cycle_ns(duty_cycle);
+		myPwm4->set_duty_cycle_ns(duty_cycle);
 	}
 }
 
-
 void test_i2c_accelerometer()
 {
-    BlackLib::BlackI2C  myI2c(BlackLib::I2C_1, 0x53);
-    bool isOpened = myI2c.open( BlackLib::ReadWrite | BlackLib::NonBlock );
-    if( !isOpened )
+    I2C  myI2c(2, 0x53);
+    if( !myI2c.open( O_RDWR  | O_NONBLOCK ))
     {
         std::cout << "I2C DEVICE CAN\'T OPEN.;" << std::endl;
         exit(1);
@@ -76,6 +77,7 @@ void test_i2c_accelerometer()
 
     // read-write register
     uint8_t powerCtl_Addr   = 0x2D;
+    uint8_t dataformat_Addr = 0x31;
 //    uint8_t offsetX_Addr    = 0x1E;
 //    uint8_t offsetY_Addr    = 0x1F;
 //    uint8_t offsetZ_Addr    = 0x20;
@@ -91,7 +93,8 @@ void test_i2c_accelerometer()
     // set bit
     uint8_t measureMode     = 0x08;
 
-    uint8_t powerCtlReg     = myI2c.readByte(powerCtl_Addr);
+    uint8_t powerCtlReg;
+    myI2c.read_byte(powerCtl_Addr, powerCtlReg);
     std::cout << "Power Ctrl's current value: " << std::hex << (int)powerCtlReg << std::dec << std::endl;
 
     powerCtlReg |= (measureMode);      //                      ___ ___ ___ ___|___ ___ ___ ___
@@ -100,25 +103,36 @@ void test_i2c_accelerometer()
                                        //                      ___ ___ ___ ___|___ ___ ___ ___ or these
                                        // result:             |_x_|_x_|_x_|_x_|_1_|_x_|_x_|_x_|
 
-    myI2c.writeByte(powerCtl_Addr, powerCtlReg);
+    myI2c.write_byte(powerCtl_Addr, powerCtlReg);
 
     std::cout << "Power Ctrl's new value: " << std::hex << (int)powerCtlReg << std::dec << std::endl;
+
+
+    uint8_t fullresMode		= 0x08;
+
+    uint8_t dataformatReg;
+    myI2c.read_byte(dataformat_Addr, dataformatReg);
+    std::cout << "data format's current value: " << std::hex << (int)dataformatReg << std::dec << std::endl;
+
+    dataformatReg |= fullresMode;
+    myI2c.write_byte(dataformat_Addr, dataformatReg);
+
+    std::cout << "data format's new value: " << std::hex << (int)dataformatReg << std::dec << std::endl;
+
 
     uint8_t values[6];
 
     while (true)
     {
-    	myI2c.readBlock(axisX0_Addr, values, sizeof(values));
+    	myI2c.read_block(axisX0_Addr, values, sizeof(values));
 
     	int X = static_cast<int16_t>(static_cast<uint16_t>(values[0]) | (static_cast<uint16_t>(values[1]) << 8));
     	int Y = static_cast<int16_t>(static_cast<uint16_t>(values[2]) | (static_cast<uint16_t>(values[3]) << 8));
     	int Z = static_cast<int16_t>(static_cast<uint16_t>(values[4]) | (static_cast<uint16_t>(values[5]) << 8));
 
     	std::cout << "X value: " << setw(10)<< X << " Y value: " << setw(10)<< Y << " Z value: " << setw(10)<< Z << std::endl;
-    	usleep(1000);
+    	usleep(20000);
     }
-
-
 }
 
 // register addresses
@@ -155,8 +169,8 @@ void test_i2c_accelerometer()
 
 void test_i2c_gyro()
 {
-	BlackLib::BlackI2C  myI2c(BlackLib::I2C_1, 0x69);
-    bool isOpened = myI2c.open( BlackLib::ReadWrite | BlackLib::NonBlock );
+	I2C  myI2c(2, 0x69);
+    bool isOpened = myI2c.open( O_RDWR | O_NONBLOCK );
     if( !isOpened )
     {
         std::cout << "I2C DEVICE CAN\'T OPEN.;" << std::endl;
@@ -166,37 +180,36 @@ void test_i2c_gyro()
     // Turns on the L3G4200D's gyro and places it in normal mode.
     // Normal power mode, all axes enabled (for detailed info see datasheet)
 
-    myI2c.writeByte(L3G4200D_CTRL_REG2, 0x00);            // highpass filter disabled
-    myI2c.writeByte(L3G4200D_CTRL_REG3, 0x00);
-    myI2c.writeByte(L3G4200D_CTRL_REG4, 0x20);            // sets acuracy to 2000 dps (degree per second)
+    myI2c.write_byte(L3G4200D_CTRL_REG2, 0x00);            // highpass filter disabled
+    myI2c.write_byte(L3G4200D_CTRL_REG3, 0x00);
+    myI2c.write_byte(L3G4200D_CTRL_REG4, 0x20);            // sets acuracy to 2000 dps (degree per second)
 
-    myI2c.writeByte(L3G4200D_CTRL_REG5, 0x00);            // deactivates the filters (only use one of these options)
+    myI2c.write_byte(L3G4200D_CTRL_REG5, 0x00);            // deactivates the filters (only use one of these options)
 
-    myI2c.writeByte(L3G4200D_CTRL_REG1, 0x0F);            // starts Gyro measurement
+    myI2c.write_byte(L3G4200D_CTRL_REG1, 0x0F);            // starts Gyro measurement
 
     uint8_t values[6];
 
     while (true)
     {
-    	usleep(100000);
-
-    	values[0] = myI2c.readByte(L3G4200D_OUT_X_L);
-    	values[1] = myI2c.readByte(L3G4200D_OUT_X_H);
+    	myI2c.read_byte(L3G4200D_OUT_X_L, values[0]);
+    	myI2c.read_byte(L3G4200D_OUT_X_H, values[1]);
 		int X = static_cast<int16_t>(static_cast<uint16_t>(values[0]) | (static_cast<uint16_t>(values[1]) << 8));
-		values[2] = myI2c.readByte(L3G4200D_OUT_Y_L);
-		values[3] = myI2c.readByte(L3G4200D_OUT_Y_H);
+		myI2c.read_byte(L3G4200D_OUT_Y_L, values[2]);
+		myI2c.read_byte(L3G4200D_OUT_Y_H, values[3]);
 		int Y = static_cast<int16_t>(static_cast<uint16_t>(values[2]) | (static_cast<uint16_t>(values[3]) << 8));
-		values[4] = myI2c.readByte(L3G4200D_OUT_Z_L);
-		values[5] = myI2c.readByte(L3G4200D_OUT_Z_H);
+		myI2c.read_byte(L3G4200D_OUT_Z_L, values[4]);
+		myI2c.read_byte(L3G4200D_OUT_Z_H, values[5]);
 		int Z = static_cast<int16_t>(static_cast<uint16_t>(values[4]) | (static_cast<uint16_t>(values[5]) << 8));
 
     	std::cout << "X value: " << setw(10)<< X << " Y value: " << setw(10)<< Y << " Z value: " << setw(10)<< Z << std::endl;
+    	usleep(1000);
     }
 }
 
 void test_i2c()
 {
-	test_i2c_accelerometer();
-//	test_i2c_gyro();
+//	test_i2c_accelerometer();
+	test_i2c_gyro();
 }
 
